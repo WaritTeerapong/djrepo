2533e18c4c3c19d89a7de554521ccc14a236ff36
WaritTeerapong <smart29952@gmail.com>
2026-03-12 11:27:25 +0700

    add create superuser in build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,12 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+# สร้าง Superuser แบบอัตโนมัติ (จะข้ามถ้ามี user นี้อยู่แล้ว)
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+  python manage.py createsuperuser \
+    --no-input \
+    --username $DJANGO_SUPERUSER_USERNAME \
+    --email $DJANGO_SUPERUSER_EMAIL
+fi
